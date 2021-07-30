321b48561399cc7872b28ddb7238b8f9f0769e67
Amrithesh <66878277+Amrithesh-Rao@users.noreply.github.com>
2021-07-31 01:55:41 +0530

    finished

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAS5OvScuLWZ745AxNXmFCzJPe19rgAA70",
+    authDomain: "spotify-clone-2d845.firebaseapp.com",
+    projectId: "spotify-clone-2d845",
+    storageBucket: "spotify-clone-2d845.appspot.com",
+    messagingSenderId: "401214882024",
+    appId: "1:401214882024:web:f02d72813d648a47a8895d",
+    measurementId: "G-8JTHPZDJML"
+  };
